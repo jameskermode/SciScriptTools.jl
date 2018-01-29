201a046df60135aca7cfdf7dc2e162d6d6de6b26
Punit Patel <p.patel@warwick.ac.uk>
2018-01-29 15:58:08 +0000

    Edit: use Base.info instead of normal print statements

--- src/Import.jl
+++ src/Import.jl
@@ -22,35 +22,31 @@ TODO: figure out way to do this from within the function
 """
 function use_package(package; repo = nothing, update = false, branch = "master" )
 
-
     # check if package already exists or is registered, clone if not
     try
         if Pkg.installed(package) == nothing
-            print("Package '$package' is registered\n")
-            print("Pkg.add($package)\n")
+            info("Package '$package' is registered\n")
+            info("Pkg.add($package)\n")
             Pkg.add(package)
         else
-            print("Package '$package' is installed\n")
+            info("Package '$package' is installed\n")
         end
     catch
-        print("Package '$package' does not exist nor is registered, trying to clone repository\n")
-        if repo == nothing
-            # put error into proper framework
-            print("Error: no repository link given!\n")
-        end
-        print("Pkg.clone($repo)\n")
+        info("Package '$package' is not installed or registered, trying to clone repository\n")
+        if repo == nothing error("No repository link given\n") end
+        info("Pkg.clone($repo)\n")
         Pkg.clone(repo)
     end
 
     if update == true
-        print("Pkg.checkout($package, $branch)\n")
+        info("Pkg.checkout($package, $branch)\n")
         Pkg.checkout(package, branch)
     end
 
     use_string = string("using", " $package")
     use = parse(use_string)
 
-    print(string(use_string, "\n"))
+    info(string(use_string, "\n"))
     eval(use)
 end
 
