ef4183493d4c893afeda6d351449fc6ab7972b96
Punit Patel <p.patel@warwick.ac.uk>
2018-06-19 15:20:59 +0100

    Edit: convert dictionary with mixed keys to keys of symbols
    separtate handling of symbols and mixed key dictionaries

--- src/Conversion.jl
+++ src/Conversion.jl
@@ -5,14 +5,16 @@ module Conversion
     """
     `dict_convert_keys(dict)`
 
-    'Converts' dictionary keys of strings/symbols to keys of symbols/strings
+    'Converts' dictionary keys of strings/symbols to keys of symbols/strings.
+    If dictionary contains mixed strings and symbols it converts all the keys to symbols.
     Returns new dictionary.
     """
     function dict_convert_keys(dict)
         
         dk = dict.keys
         if typeof(dk) == Array{String,1} t = "t_string" end
-        if typeof(dk) == Array{Any,1} t = "t_symbol" end # using type Symbols creates dictionary of Array{Any,1}
+        if typeof(dk) == Array{Symbol,1} t = "t_symbol" end
+        if typeof(dk) == Array{Any,1} t = "t_mixed" end # check if keys are mix of strings and symbols
         
         dict_c = Dict()
         for i in 1:length(dk)
@@ -21,6 +23,7 @@ module Conversion
             try
                 if t == "t_string" dict_c[Symbol(dk[i])] = dict[dk[i]] end
                 if t == "t_symbol" dict_c[String(dk[i])] = dict[dk[i]] end
+                if t == "t_mixed" dict_c[Symbol(dk[i])] = dict[dk[i]] end # convert mixed keys into symbols
             catch
             end
         end
