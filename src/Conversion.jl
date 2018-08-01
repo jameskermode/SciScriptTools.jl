579d8eb1da558c20f6661eb6c913393a21785e76
Punit Patel <p.patel@warwick.ac.uk>
2018-08-01 11:33:37 +0100

    Add: function to flatten tuple arrays into single list

--- src/Conversion.jl
+++ src/Conversion.jl
@@ -1,6 +1,6 @@
 module Conversion
 
-    export dict_convert_keys
+    export dict_convert_keys, pair_to_list
 
     """
     `dict_convert_keys(dict)`
@@ -29,6 +29,43 @@ module Conversion
         end
 
         return dict_c 
+    end
+
+    """
+    `pair_to_list(pairs::Array{Tuple}; split = false)`
+
+    Takes array of tuples and returns flatten array.
+
+    ### Arugments
+    - `pairs::Array{Tuple}` : tuple array of any type 
+    - `split = false` : `false` flattens tuple by tuple,
+                        `true` returns with first index of all tuples than second index of all tuples
+    """
+    pair_to_list(pair::Tuple) = [pair[1], pair[2]]
+    function pair_to_list(pairs::Array{Tuple}; split = false)
+
+        len = length(pairs)
+        list = nothing
+        
+        # initialise array with type
+        if typeof(pairs[1][1]) == typeof(pairs[1][2]) list = Array{typeof(pairs[1])}(len*2)
+        else list =Array{Any}(len*2) end
+            
+        # flatten tuples one by one
+        if split == false
+            for i in 1:len
+                list[2*i - 1] = pairs[i][1]
+                list[2*i] = pairs[i][2]
+            end
+        # flatten tuples separating the all the first indices and second indices
+        elseif split == true
+            j = len
+            for i in 1:len
+                list[i] = pairs[i][1]
+                list[j+i] = pairs[i][2]
+            end
+        else error("split arguement not boolean") end
+        return list
     end
 
 end # module
